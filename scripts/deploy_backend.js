da14219a8860237240f3477b2156d895228e6444
Sebastian Blanco <sblanco@devartis.com>
2020-01-20 15:04:25 -0300

    Do not remove the folder because it has de db data

--- scripts/deploy_backend.js
+++ scripts/deploy_backend.js
@@ -14,9 +14,7 @@ const branch = config.branch;
 const sshAddress = config.ssh_address;
 const containerName = config.container_name;
 
-shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} rm -rf ${location}`);
 shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} git clone -b ${branch} ${repository} ${location}`);
 shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'cd ${location} && docker-compose up -d --build'`);
 shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn db:create`);
 shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn db:migrate`);
-shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} rm -rf ${location}`);
