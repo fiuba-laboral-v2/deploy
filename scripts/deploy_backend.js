9cd0cf497207b5fc92e3dc64e9f200dd4b14a265
Sebastian Blanco <sblanco@devartis.com>
2020-01-19 21:15:23 -0300

    Add container name as variable

--- scripts/deploy_backend.js
+++ scripts/deploy_backend.js
@@ -12,10 +12,11 @@ const location = config.location;
 const repository = config.repository;
 const branch = config.branch;
 const sshAddress = config.ssh_address;
+const containerName = config.container_name;
 
 shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} rm -rf ${location}`);
 shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} git clone -b ${branch} ${repository} ${location}`);
 shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'cd ${location} && docker-compose up --build'`);
-shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec fiuba-laboral-v2-backend yarn db:create`);
-shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec fiuba-laboral-v2-backend yarn db:migrate`);
+shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn db:create`);
+shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn db:migrate`);
 shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} rm -rf ${location}`);
