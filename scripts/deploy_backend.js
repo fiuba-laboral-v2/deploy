90d3cd974cfd0552eec4164a1fa86a02ef15e22b
Sebastian Blanco <sblanco@devartis.com>
2020-01-20 19:54:44 -0300

    Add messages

--- scripts/deploy_backend.js
+++ scripts/deploy_backend.js
@@ -14,12 +14,22 @@ const branch = config.branch;
 const sshAddress = config.ssh_address;
 const containerName = config.container_name;
 
-if (shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress}cd ${location}`).code !== 0) {
+if (shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} cd ${location}`).code !== 0) {
+    shell.echo(`git clone -b ${branch} ${repository} ${location}`);
     shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} git clone -b ${branch} ${repository} ${location}`);
+
+    shell.echo("building container");
     shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'cd ${location} && docker-compose up -d --build'`);
+
+    shell.echo("Creating database");
     shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn db:create`);
 } else {
+    shell.echo(`cd ${location} && git pull origin ${branch}`);
     shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} cd ${location} && git pull origin ${branch}`);
+
+    shell.echo(`docker exec ${containerName} yarn install`);
     shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn install`);
 }
+shell.echo(`docker exec ${containerName} yarn db:migrate`);
 shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn db:migrate`);
+
