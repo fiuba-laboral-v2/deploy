affe40a4f70be6661efd8d2853f0c8314e368e0b
Sebastian Blanco <sblanco@devartis.com>
2020-01-20 02:37:05 -0300

    Add setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo ufw allow 'Apache'
+sudo ufw enable
+
+echo "creating '/var/www/$HOSTNAME/html' folder"
+sudo mkdir -p "/var/www/$HOSTNAME/html"
+
+echo "creating user permissions for the '/var/www/$HOSTNAME/html' folder"
+sudo chown -R "$USER:$USER" "/var/www/$HOSTNAME/html"
+sudo chmod -R 755 "/var/www/$HOSTNAME"
+
+echo "creating apache cofiguration"
+HOSTNAME=$HOSTNAME bash scripts/set_default_settings.sh
+
+echo "Set up docker permissions"
+sudo groupadd docker
+sudo usermod -aG docker $USER
