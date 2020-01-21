e11aeaa4802076ea0e9820a7d21a11649f8aeb68
Sebastian Blanco <sblanco@devartis.com>
2020-01-21 01:55:38 -0300

    Add message to code failure

--- scripts/deploy_backend.js
+++ scripts/deploy_backend.js
@@ -16,27 +16,46 @@ const containerName = config.container_name;
 
 let code;
 
+
 if (shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} cd ${location}`).code !== 0) {
     shell.echo(`git clone -b ${branch} ${repository} ${location}`);
     code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} git clone -b ${branch} ${repository} ${location}`);
-    if (code !== 0) return code;
+    if (code !== 0) {
+        shell.echo(`FAILED with code ${code}`);
+        return code;
+    }
 
     shell.echo("building container");
     code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'cd ${location} && docker-compose up -d --build'`);
-    if (code !== 0) return code;
+    if (code !== 0) {
+        shell.echo(`FAILED with code ${code}`);
+        return code;
+    }
 
     shell.echo("Creating database");
     code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn db:create`);
-    if (code !== 0) return code;
+    if (code !== 0) {
+        shell.echo(`FAILED with code ${code}`);
+        return code;
+    }
 } else {
     shell.echo(`cd ${location} && git pull origin ${branch}`);
     code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'cd ${location} && git pull origin ${branch}'`);
-    if (code !== 0) return code;
+    if (code !== 0) {
+        shell.echo(`FAILED with code ${code}`);
+        return code;
+    }
 
     shell.echo("building container");
     code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'cd ${location} && docker-compose up -d --build'`);
-    if (code !== 0) return code;
+    if (code !== 0) {
+        shell.echo(`FAILED with code ${code}`);
+        return code;
+    }
 }
 shell.echo(`docker exec ${containerName} yarn db:migrate`);
 code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn db:migrate`);
-if (code !== 0) return code;
+if (code !== 0) {
+    shell.echo(`FAILED with code ${code}`);
+    return code;
+}
