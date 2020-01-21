461a033adb7aa5150a9339fedbc0870880a65263
Sebastian Blanco <sblanco@devartis.com>
2020-01-21 02:36:31 -0300

    Use exit

--- scripts/deploy_frontend.js
+++ scripts/deploy_frontend.js
@@ -21,16 +21,18 @@ code = shell.exec(`rm -rf ${location}`).code;
 if (code !== 0) return code;
 
 code = shell.exec(`git clone -b ${branch} ${repository} ${location}`).code;
-if (code !== 0) return code;
+if (code !== 0) shell.exit(code);
 
 code = shell.exec(`cd ${location} &&  yarn install && PUBLIC_URL=${publicURL} yarn build`).code;
-if (code !== 0) return code;
+if (code !== 0) shell.exit(code);
 
 code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} rm -rf /var/www/${hostname}/html/*`).code;
-if (code !== 0) return code;
+if (code !== 0) shell.exit(code);
 
 code = shell.exec(`scp -o "StrictHostKeyChecking no" -r ${location}/build/* ${sshAddress}:/var/www/${hostname}/html/`).code;
-if (code !== 0) return code;
+if (code !== 0) shell.exit(code);
 
 code = shell.exec(`rm -rf ${location}`).code;
-if (code !== 0) return code;
+if (code !== 0) shell.exit(code);
+
+shell.exit(0);
