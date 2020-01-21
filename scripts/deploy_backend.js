660db9a66603efe1699725d24fcd66d740370365
Sebastian Blanco <sblanco@devartis.com>
2020-01-21 01:48:26 -0300

    Add code to stop CD if it fails

--- scripts/deploy_backend.js
+++ scripts/deploy_backend.js
@@ -14,22 +14,29 @@ const branch = config.branch;
 const sshAddress = config.ssh_address;
 const containerName = config.container_name;
 
+let code;
+
 if (shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} cd ${location}`).code !== 0) {
     shell.echo(`git clone -b ${branch} ${repository} ${location}`);
-    shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} git clone -b ${branch} ${repository} ${location}`);
+    code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} git clone -b ${branch} ${repository} ${location}`);
+    if (code !== 0) return code;
 
     shell.echo("building container");
-    shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'cd ${location} && docker-compose up -d --build'`);
+    code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'cd ${location} && docker-compose up -d --build'`);
+    if (code !== 0) return code;
 
     shell.echo("Creating database");
-    shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn db:create`);
+    code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn db:create`);
+    if (code !== 0) return code;
 } else {
     shell.echo(`cd ${location} && git pull origin ${branch}`);
-    shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'cd ${location} && git pull origin ${branch}'`);
+    code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'cd ${location} && git pull origin ${branch}'`);
+    if (code !== 0) return code;
 
     shell.echo("building container");
-    shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'cd ${location} && docker-compose up -d --build'`);
+    code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'cd ${location} && docker-compose up -d --build'`);
+    if (code !== 0) return code;
 }
 shell.echo(`docker exec ${containerName} yarn db:migrate`);
-shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn db:migrate`);
-
+code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} docker exec ${containerName} yarn db:migrate`);
+if (code !== 0) return code;
