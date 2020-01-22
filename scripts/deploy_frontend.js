c88419a53913bf34d6aa59abdcd1d82e27c7f39b
Sebastian Blanco <sblanco@devartis.com>
2020-01-22 02:55:37 -0300

    Use functions to reduce code repetition

--- scripts/deploy_frontend.js
+++ scripts/deploy_frontend.js
@@ -8,31 +8,65 @@ if (config === undefined) {
     shell.exit(1);
 }
 
+function sshCommand(sshAddress) {
+    return `ssh -o "StrictHostKeyChecking no" ${sshAddress}`;
+}
+
+function throwErrorIfFails(code) {
+    if (code !== 0 ) throw { code };
+}
+
+function removeRepository(gitRepository) {
+    const command = `rm -rf ${gitRepository.location}`;
+    shell.echo(command);
+    const code = shell.exec(command).code;
+    throwErrorIfFails(code);
+}
+
+function cloneRepository(sshAddress, gitRepository) {
+    const command = `git clone -b ${gitRepository.branch} ${gitRepository.url} ${gitRepository.location}`;
+    shell.echo(command);
+    const code = shell.exec(`${command}`).code;
+    throwErrorIfFails(code);
+}
+
+function buildHtml(gitRepository, publicURL) {
+    const goToRepositoryLocation = `cd ${gitRepository.location}`;
+    const installDependencies = "yarn install";
+    const build = `PUBLIC_URL=${publicURL} yarn build`;
+    const command = `${goToRepositoryLocation} && ${installDependencies} && ${build}`;
+    shell.echo(command);
+    const code = shell.exec(command).code;
+    throwErrorIfFails(code);
+}
+
+function removeServedHtml(sshAddress, hostname) {
+    const command = `${sshCommand(sshAddress)} rm -rf /var/www/${hostname}/html/*`;
+    shell.echo(command);
+    const code = shell.exec(command).code;
+    throwErrorIfFails(code);
+}
+
+function deployHtml(sshAddress, gitRepository, hostname) {
+    const command = `scp -o "StrictHostKeyChecking no" -r ${gitRepository.location}/build/* ${sshAddress}:/var/www/${hostname}/html/`;
+    shell.echo(command);
+    const code = shell.exec(command).code;
+    throwErrorIfFails(code);
+}
+
+const gitRepository = config.git_repository;
 const hostname = config.hostname;
-const location = config.location;
-const repository = config.repository;
-const branch = config.branch;
-const publicURL = config.PUBLIC_URL;
+const publicURL = config.public_url;
 const sshAddress = config.ssh_address;
 
-let code;
-
-code = shell.exec(`rm -rf ${location}`).code;
-if (code !== 0) return code;
-
-code = shell.exec(`git clone -b ${branch} ${repository} ${location}`).code;
-if (code !== 0) shell.exit(code);
-
-code = shell.exec(`cd ${location} &&  yarn install && PUBLIC_URL=${publicURL} yarn build`).code;
-if (code !== 0) shell.exit(code);
-
-code = shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} rm -rf /var/www/${hostname}/html/*`).code;
-if (code !== 0) shell.exit(code);
-
-code = shell.exec(`scp -o "StrictHostKeyChecking no" -r ${location}/build/* ${sshAddress}:/var/www/${hostname}/html/`).code;
-if (code !== 0) shell.exit(code);
-
-code = shell.exec(`rm -rf ${location}`).code;
-if (code !== 0) shell.exit(code);
-
-shell.exit(0);
+try {
+    removeRepository(gitRepository);
+    cloneRepository(sshAddress, gitRepository);
+    buildHtml(gitRepository, publicURL);
+    removeServedHtml(sshAddress, hostname);
+    deployHtml(sshAddress, gitRepository, hostname);
+    removeRepository(gitRepository);
+    return shell.exit(0);
+} catch (e) {
+    return shell.exit(e.code);
+}
