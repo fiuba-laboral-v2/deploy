012a55b95b52bc98d69bab1cbe51f02bf80c1fef
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-01 05:25:12 -0300

    Test GitManager

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  coveragePathIgnorePatterns: ["node_modules", "test"],
+  coveragePathIgnorePatterns: ["node_modules", "test", "fiuba-laboral-v2"],
   globals: {
     "ts-jest": {
       isolatedModules: true
     }
   },
-  watchPathIgnorePatterns: ["/node_modules/"],
+  watchPathIgnorePatterns: ["./node_modules/", "./fiuba-laboral-v2/**/*.ts"],
   testPathIgnorePatterns: [".d.ts", ".js"],
   setupFiles: ["core-js"]
 };
