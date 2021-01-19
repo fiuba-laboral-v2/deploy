96434e6dbf893a3ba7b879c3461c8871d20c735e
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-18 23:55:44 -0300

    Migrate setup to typescript

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,7 +11,7 @@ sudo chown -R "$USER:$USER" "/var/www/$HOSTNAME/html"
 sudo chmod -R 755 "/var/www/$HOSTNAME"
 
 echo "creating apache configuration"
-HOSTNAME=$HOSTNAME FRONTEND_PATH=$FRONTEND_PATH sh fiuba-laboral-v2/scripts/set_default_settings.sh
+HOSTNAME=$HOSTNAME FRONTEND_PATH=$FRONTEND_PATH bash fiuba-laboral-v2/scripts/set_default_settings.sh
 
 echo "Set up docker permissions"
 sudo groupadd docker
