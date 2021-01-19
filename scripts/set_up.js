71019666ba190231d2336648ce18e82cb6b63eff
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-18 23:44:56 -0300

    Copy setup files to the ssh server

--- scripts/set_up.js
+++ scripts/set_up.js
@@ -6,4 +6,6 @@ const frontendPath = Config.frontendPath;
 const user = Config.user;
 const sshAddress = Config.sshAddress;
 
+shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'mkdir $HOME/scripts/'`);
+shell.exec(`scp -o "StrictHostKeyChecking no" -r ./scripts/* ${sshAddress}:$HOME/scripts/`);
 shell.exec(`ssh -o "StrictHostKeyChecking no" -tt ${sshAddress} 'USER=${user} HOSTNAME=${hostname} FRONTEND_PATH=${frontendPath} bash scripts/setup.sh'`);
