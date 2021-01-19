8ccd575409bd11ad96c1ef23f2d01f97f468ca12
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-18 23:46:09 -0300

    Change scripts path

--- scripts/set_up.js
+++ scripts/set_up.js
@@ -6,6 +6,6 @@ const frontendPath = Config.frontendPath;
 const user = Config.user;
 const sshAddress = Config.sshAddress;
 
-shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'mkdir $HOME/scripts/'`);
-shell.exec(`scp -o "StrictHostKeyChecking no" -r ./scripts/* ${sshAddress}:$HOME/scripts/`);
-shell.exec(`ssh -o "StrictHostKeyChecking no" -tt ${sshAddress} 'USER=${user} HOSTNAME=${hostname} FRONTEND_PATH=${frontendPath} bash scripts/setup.sh'`);
+shell.exec(`ssh -o "StrictHostKeyChecking no" ${sshAddress} 'mkdir $HOME/fiuba-laboral-v2/scripts/'`);
+shell.exec(`scp -o "StrictHostKeyChecking no" -r ./scripts/* ${sshAddress}:$HOME/fiuba-laboral-v2/scripts/`);
+shell.exec(`ssh -o "StrictHostKeyChecking no" -tt ${sshAddress} 'USER=${user} HOSTNAME=${hostname} FRONTEND_PATH=${frontendPath} bash fiuba-laboral-v2/scripts/setup.sh'`);
