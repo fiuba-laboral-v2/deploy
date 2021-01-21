716ace7058f27003bd880f7f6c1034efb97fb58a
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-20 23:48:33 -0300

    Move logic to the setup file

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,22 +1,39 @@
 import { DeployConfig } from "../src/config/deploy";
-import shell from "shelljs";
+import { GitManager } from "../src/models/GitManager";
+import { BackendConfig } from "../src/config/Repository";
+import { BackendManager } from "../src/models/BackendManager";
+import { Environment } from "../src/config";
+import { Shell } from "../src/models/Shell";
+import { SSHManager } from "../src/models/SSHManager";
 
 const hostname = DeployConfig.hostname;
 const frontendPath = DeployConfig.frontendPath;
 const user = DeployConfig.user;
 const sshAddress = DeployConfig.sshAddress;
+const { gitRepository: repositoryConfig } = BackendConfig;
 
-const sshKeyArgument = "-o \"StrictHostKeyChecking no\"";
-shell.exec(`ssh ${sshKeyArgument} ${sshAddress} 'mkdir -p $HOME/fiuba-laboral-v2/scripts/'`);
-shell.exec(`scp ${sshKeyArgument} -r ./scripts/* ${sshAddress}:$HOME/fiuba-laboral-v2/scripts/`);
-shell.exec(
-  `
-  ssh ${sshKeyArgument} -tt ${sshAddress} \
-  '
+const gitManager = new GitManager({ repositoryConfig, withSSHConnection: true });
+const backendManager = new BackendManager(Environment.NODE_ENV());
+const scriptsFolder = "fiuba_laboral_v2_scripts";
+const destinationDirectory = `${sshAddress}:~/${scriptsFolder}`;
+
+Shell.execute({ command: SSHManager.copy({ sourceDirectory: "scripts", destinationDirectory }) });
+Shell.execute({
+  command: SSHManager.command(
+    `
     USER=${user}
     HOSTNAME=${hostname}
     FRONTEND_PATH=${frontendPath}
-    bash fiuba-laboral-v2/scripts/setup.sh
-  '
-`
-);
+    bash ~/${scriptsFolder}/setup.sh
+  `
+  )
+});
+
+const isFirstDeploy = gitManager.repositoryWasNotCloned();
+if (isFirstDeploy) gitManager.cloneRepository();
+if (isFirstDeploy) backendManager.initializeEnvFile();
+
+Shell.execute({
+  label: "finished! removing scripts from server",
+  command: `rm -rf ~/${scriptsFolder}`
+});
